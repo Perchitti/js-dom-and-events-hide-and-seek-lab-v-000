a11b31d81529d58370e6381acd2bb2b13772a1f3
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-02 20:03:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function deepestChild(){
 }
 
 function increaseRankBy(n){
-  const rankedLists = document.querySelectorAll('.ranked-list.')
+  const rankedLists = document.getElementById('.app').querySelectorAll('ul.ranked-list li')
   for (let i = 0; i < rankedLists.length, i++){
     rankedLists[i] = parseInt(rankedLists[i].innerHTML, 10) + n
   }
