ee0f78ced1bd566603d2d3df56ef81801e4f3e0e
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-02 19:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const getFirstSelector = document.querySelector('.ranked-list')
+const div = getFirstSelector[0]
 
 
 const nestedTarget = document.getElementById('#nested').querySelector('.target')
